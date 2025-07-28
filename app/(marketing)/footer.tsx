0508e7251a221b3eb0f647511206fb443e0070a1
parkminjo <cruiser367@naver.com>
2025-07-28 15:04:38 +0900

    style: footer 생성

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
+      Footer
+    </footer>
+  );
+};
+
+export default Footer;
