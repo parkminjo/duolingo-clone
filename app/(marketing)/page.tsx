1cc4492835cc84ef85f47c4d35ace1702776108d
parkminjo <cruiser367@naver.com>
2025-07-28 15:56:50 +0900

    style: 렌딩페이지 구현

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,53 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
+      <div className="relative mb-8 h-[240px] w-[240px] lg:mb-0 lg:h-[240px] lg:w-[240px]">
+        <Image src="/mascot.svg" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="max-w-[480px] text-center text-xl font-bold text-neutral-600 lg:text-3xl">
+          Learn, practice, and master new languages with Lingo
+        </h1>
+        <ClerkLoading>
+          <Loader className="text-muted-foreground h-5 w-5 animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <div className="flex w-full flex-col gap-3">
+              <SignUpButton>
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton>
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </div>
+          </SignedOut>
+          <SignedIn>
+            <Button size="lg" variant="secondary" className="w-full" asChild>
+              <Link href="/learn">Continue Learning</Link>
+            </Button>
+          </SignedIn>
+        </ClerkLoaded>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
