aa67b52326f0457f75390a2833023739a190faa7
parkminjo <cruiser367@naver.com>
2025-07-29 14:20:33 +0900

    style: FeedWrapper 구현

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,11 @@
+import FeedWrapper from "@/components/common/feed-wrapper";
 import StickyWrapper from "@/components/common/sticky-wrapper";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>Sticky sidebar</StickyWrapper>
+      <FeedWrapper>my feed</FeedWrapper>
     </div>
   );
 };
---
+++ components/common/feed-wrapper.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+const FeedWrapper = ({ children }: Props) => {
+  return <div className="relative top-0 flex-1 pb-10">{children}</div>;
+};
+
+export default FeedWrapper;
