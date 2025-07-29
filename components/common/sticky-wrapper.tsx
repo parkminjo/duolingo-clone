5b73cdb90f52120bb392c83ad723ac7ba3296f7b
parkminjo <cruiser367@naver.com>
2025-07-29 14:14:33 +0900

    style: bottom값 수정

--- components/common/sticky-wrapper.tsx
+++ components/common/sticky-wrapper.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const StickyWrapper = ({ children }: Props) => {
   return (
-    <div className="sticky bottom-0 hidden w-[368px] self-end lg:block">
+    <div className="sticky bottom-6 hidden w-[368px] self-end lg:block">
       <div className="sticky top-6 flex min-h-[cal(100vh-48px)] flex-col gap-y-4">
         {children}
       </div>
