f92b848713015031a1343ffa6d68820413320a1a
parkminjo <cruiser367@naver.com>
2025-07-29 14:08:23 +0900

    style: StickyWrapper 구현

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,11 @@
+import StickyWrapper from "@/components/common/sticky-wrapper";
+
 const LearnPage = () => {
-  return <div>LearnPage</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>Sticky sidebar</StickyWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
---
+++ components/common/sticky-wrapper.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+const StickyWrapper = ({ children }: Props) => {
+  return (
+    <div className="sticky bottom-0 hidden w-[368px] self-end lg:block">
+      <div className="sticky top-6 flex min-h-[cal(100vh-48px)] flex-col gap-y-4">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default StickyWrapper;
